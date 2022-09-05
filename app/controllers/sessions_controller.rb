e4fefed57a3bbffdf228b989f3c7a7124afb7d3a
Joseph Guillory <jagmanx99@yahoo.com>
2022-09-04 20:40:49 -0700

    Requests work

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user, status: :created
+        else
+        render json: { error: "Not authorized" }, status: :unauthorized
+      end
+    end
+
+end
