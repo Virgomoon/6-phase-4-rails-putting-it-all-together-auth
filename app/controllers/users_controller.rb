2fa18b0c8bd756c3ea6240447b8c1547b8443bb9
Joseph Guillory <jagmanx99@yahoo.com>
2022-09-05 19:35:01 -0700

    Two tests to go

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,20 @@
 class UsersController < ApplicationController
+
+    skip_before_action :authorize, only: :create
+
     def create
-        user = User.create(user_params)
-        if user.valid?
+        user = User.create!(user_params)
         session[:user_id] = user.id
         render json: user, status: :created
-        else
-        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-        end
     end
 
     def show
-        user = User.find_by(id: session[:user_id])
-        if user
-          render json: user
-        else
-          render json: { error: "Not authorized" }, status: :unauthorized
-        end
+        render json: @current_user
     end
 
     private
 
   def user_params
-    params.permit(:username, :password, :password_confirmation)
+    params.permit(:username, :password, :password_confirmation, :image_url, :bio)
   end
 end
